a47c5c034ebea3feaad32deb5f7ae36f27e7b869
langhf <282298978@qq.com>
2019-02-02 21:13:45 +0800

    reverse integer

---
+++ src/main/java/com/drelang/algorithms/reverseInteger/Solution.java
@@ -0,0 +1,5 @@
+package com.drelang.algorithms.reverseInteger;
+
+public abstract class Solution {
+        public abstract int reverse(int x);
+}
--- src/main/java/com/drelang/algorithms/validParentheses/SolutionA.java
+++ src/main/java/com/drelang/algorithms/validParentheses/SolutionA.java
@@ -2,6 +2,17 @@ package com.drelang.algorithms.validParentheses;
 
 import java.util.Stack;
 
+/**
+  *  1. 初始化一个 Stack ，用来存放 Character
+ *   2. 自头至尾遍历字符串取字符，
+ *      若 Stack 为空，直接添加该字符；
+ *      若 Stack 不为空，判断 Stack 顶部字符是否与该字符互补
+ *              若互补，则弹出顶部字符
+ *              若不互补，则加入该字符至 Stack 中
+ *   3. Stack 为空则是有效的括弧， 否则不是有效的括弧。
+ *
+  * Created by Drelang on 2019/02/02
+  */
 public class SolutionA extends Solution {
     @Override
     public boolean isValid(String s) {
