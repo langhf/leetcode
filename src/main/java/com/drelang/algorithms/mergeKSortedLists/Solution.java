c2234e06eb5440e288235c729dfebb92403c7688
langhf <282298978@qq.com>
2019-02-09 17:10:48 +0800

    merge k sorted lists

---
+++ src/main/java/com/drelang/algorithms/mergeKSortedLists/Solution.java
@@ -0,0 +1,9 @@
+package com.drelang.algorithms.mergeKSortedLists;
+
+
+/**
+ * Created by Drelang on 2019/02/07 22:38
+ */
+public abstract class Solution {
+    public abstract ListNode mergeKLists(ListNode[] lists);
+}
