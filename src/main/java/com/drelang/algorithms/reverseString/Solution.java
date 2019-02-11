7c469ee05fc146280cc688d147b507661a9148bf
langhf <282298978@qq.com>
2019-02-11 19:23:19 +0800

    reverse string

---
+++ src/main/java/com/drelang/algorithms/reverseString/Solution.java
@@ -0,0 +1,25 @@
+package com.drelang.algorithms.reverseString;
+
+/**
+ * Write a function that reverses a string. The input string is given as an array of characters char[].
+ *
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ *
+ * You may assume all the characters consist of printable ascii characters.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ * Example 2:
+ *
+ * Input: ["H","a","n","n","a","h"]
+ * Output: ["h","a","n","n","a","H"]
+ *
+ * Created by Drelang on 2019/02/11 17:59
+ */
+public abstract class Solution {
+    public abstract void reverseString(char[] s);
+}
---
+++ src/main/java/com/drelang/algorithms/reverseString/TestSolution.java
@@ -0,0 +1,18 @@
+package com.drelang.algorithms.reverseString;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Drelang on 2019/02/11 18:09
+ */
+public class TestSolution {
+    @Test
+    public void testSolutionA() {
+        char[] s = { 'h', 'e', 'l', 'l', 'o'};
+        char[] expected = {'o', 'l', 'l', 'e', 'h'};
+        Solution solution = new SolutionA();
+        solution.reverseString(s);
+        Assert.assertArrayEquals(expected, s);
+    }
+}
