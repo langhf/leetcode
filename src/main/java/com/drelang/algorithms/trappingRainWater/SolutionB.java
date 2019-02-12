26742eeef04822e073c54e10e42f35678350efa0
langhf <282298978@qq.com>
2019-02-12 21:31:39 +0800

    trapping rain water

---
+++ src/main/java/com/drelang/algorithms/trappingRainWater/SolutionB.java
@@ -0,0 +1,39 @@
+package com.drelang.algorithms.trappingRainWater;
+
+import com.drelang.algorithms.mergeTwoSortedLists.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 动态规划
+ *
+ * 用容器保存每个块的最高左块和最高右块
+ *
+ * Created by Drelang on 2019/02/12 18:26
+ */
+public class SolutionB extends Solution {
+    @Override
+    public int trap(int[] height) {
+        int n = height.length;
+        if(n<2) return 0;
+        int[] maxLeft = new int[n];
+        int[] maxRight = new int[n];
+        // 存储最高左块
+        maxLeft[0] = height[0];
+        for(int i=1; i<n; i++) {
+            maxLeft[i] = Math.max(maxLeft[i-1], height[i]);
+        }
+        // 存储最高右块
+        maxRight[n-1] = height[n-1];
+        for(int i=n-2; i>=0; i--) {
+            maxRight[i] = Math.max(maxRight[i+1], height[i]);
+        }
+        // 计算总蓄水量
+        int ans = 0;
+        for(int i=1; i<n-1; i++) {
+            ans += Math.min(maxLeft[i], maxRight[i]) - height[i];
+        }
+        return ans;
+    }
+}
---
+++ src/main/java/com/drelang/algorithms/trappingRainWater/TestSolution.java
@@ -0,0 +1,21 @@
+package com.drelang.algorithms.trappingRainWater;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Drelang on 2019/02/12 18:02
+ */
+public class TestSolution {
+    @Test
+    public void testSolutionA() {
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        int expected = 6;
+        Solution solution = new SolutionA();
+        Assert.assertEquals(expected, solution.trap(height));
+        Solution solutionB = new SolutionB();
+        Assert.assertEquals(expected, solutionB.trap(height));
+        Solution solutionC = new SolutionC();
+        Assert.assertEquals(expected, solutionC.trap(height));
+    }
+}
