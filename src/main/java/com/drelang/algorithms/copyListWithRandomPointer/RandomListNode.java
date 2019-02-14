bab92e3249d7d60c47a98334ff44a588888ec7f2
langhf <282298978@qq.com>
2019-02-14 21:33:28 +0800

    copy list with random pointer

---
+++ src/main/java/com/drelang/algorithms/copyListWithRandomPointer/RandomListNode.java
@@ -0,0 +1,10 @@
+package com.drelang.algorithms.copyListWithRandomPointer;
+
+/**
+ * Created by Drelang on 2019/02/13 15:32
+ */
+public class RandomListNode {
+     int label;
+     RandomListNode next, random;
+     RandomListNode(int x) { this.label = x; };
+}
