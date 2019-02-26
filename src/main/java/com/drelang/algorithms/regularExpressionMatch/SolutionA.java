822c1c12c0b6c0015557a7d63151b71adaff4c6c
langhf <282298978@qq.com>
2019-02-26 21:51:47 +0800

    regular expression match

--- src/main/java/com/drelang/algorithms/regularExpressionMatch/SolutionA.java
+++ src/main/java/com/drelang/algorithms/regularExpressionMatch/SolutionA.java
@@ -3,5 +3,17 @@ package com.drelang.algorithms.regularExpressionMatch;
 /**
  * Created by Drelang on 2019/02/15 15:42
  */
-public class SolutionA {
+public class SolutionA extends Solution {
+    @Override
+    public boolean isMatch(String s, String p) {
+        if(p.isEmpty()) return  s.isEmpty();
+        boolean firstMatch = ( !s.isEmpty() &&
+                                                ( p.charAt(0) == s.charAt(0) || p.charAt(0) == '.') );
+        if( p.length() >= 2 && p.charAt(1) == '*') {
+            return isMatch(s, p.substring(2)) || (firstMatch && isMatch(s.substring(1), p));
+        }
+        else {
+            return firstMatch && isMatch(s.substring(1), p.substring(1));
+        }
+    }
 }
