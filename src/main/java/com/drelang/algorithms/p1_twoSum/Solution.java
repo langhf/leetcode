944518fa80ffafa1d444455412d74decc860fdaa
langhf <282298978@qq.com>
2019-05-20 21:28:39 +0800

    p1_twoSum

---
+++ src/main/java/com/drelang/algorithms/p1_twoSum/Solution.java
@@ -0,0 +1,38 @@
+package com.drelang.algorithms.p1_twoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ *
+ * Created by Drelang on 2019/05/20 21:14
+ */
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            if (map.isEmpty()) {
+                map.put(nums[i], i);
+            } else {
+                if (map.containsKey(target-nums[i])) {
+                    return new int[]{i, map.get(target-nums[i])};
+                } else {
+                    map.put(nums[i], i);
+                }
+            }
+        }
+        return null;
+    }
+}
+
